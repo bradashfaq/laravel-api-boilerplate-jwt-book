5a1abf362954e9b861f74ad1a6d2524a7a6b4186
Tony <Tony133@users.noreply.github.com>
2020-05-17 16:14:25 +0200

    Fix Category Factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -8,6 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(Category::class, function (Faker $faker) {
     return [
         'name' => $faker->sentence(),
-        'book_id' => App\Models\Book::pluck('id')->random()
+        'book_id' => App\Book::pluck('id')->random()
     ];
 });
